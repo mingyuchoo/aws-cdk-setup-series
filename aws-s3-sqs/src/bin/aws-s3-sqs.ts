031344d984ca63232a94e527014bb0424e586f80
mingyuchoo <mingyuchoo@gmail.com>
2023-08-06 09:30:02 +0900

    updated

--- aws-s3-sqs/src/bin/aws-s3-sqs.ts
+++ aws-s3-sqs/src/bin/aws-s3-sqs.ts
@@ -5,6 +5,7 @@ import { AwsS3SqsStack } from '../lib/aws-s3-sqs-stack';
 
 const app = new cdk.App();
 new AwsS3SqsStack(app, 'AwsS3SqsStack', {
+  stackName: 'AwsS3SqsStack',
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION
